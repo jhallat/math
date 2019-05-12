b225513698e8b4d9d5892997bd5fbd3ebe73fef7
John Hallat <jhallat@gmail.com>
2019-05-12 10:53:21 -0400

    updated organization

---
+++ math/combinatorics/src/main/java/com/jhallat/combinatorics/Combinatorics.java
@@ -0,0 +1,10 @@
+package com.jhallat.combinatorics;
+
+public class Combinatorics {
+
+	
+	public static int permutations(int n, int m) {
+		return 0;
+	}
+	
+}
---
+++ math/combinatorics/src/test/java/com/jhallat/combinatorics/test/PermutationTest.java
@@ -0,0 +1,5 @@
+package com.jhallat.combinatorics.test;
+
+public class PermutationTest {
+
+}
