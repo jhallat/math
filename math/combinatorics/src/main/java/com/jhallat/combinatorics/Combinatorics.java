156158d4c70b5626976618e77166c3568ec77df9
John Hallat <jhallat@gmail.com>
2019-05-19 13:41:22 -0400

    added permutation test

--- math/combinatorics/src/main/java/com/jhallat/combinatorics/Combinatorics.java
+++ math/combinatorics/src/main/java/com/jhallat/combinatorics/Combinatorics.java
@@ -2,9 +2,21 @@ package com.jhallat.combinatorics;
 
 public class Combinatorics {
 
-	
+	//TODO confirm n > m
 	public static int permutations(int n, int m) {
-		return 0;
+	
+		return factorial(n) / factorial(n - m);
 	}
 	
+
+	//TODO Confirm positive number
+	private static int factorial(int n) {
+		
+		if (n == 1 || n == 0) {
+			return 1;
+		} 
+		
+		return n * factorial(n - 1);
+		
+	}
 }
--- math/combinatorics/src/test/java/com/jhallat/combinatorics/test/PermutationTest.java
+++ math/combinatorics/src/test/java/com/jhallat/combinatorics/test/PermutationTest.java
@@ -1,5 +1,34 @@
 package com.jhallat.combinatorics.test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.jhallat.combinatorics.Combinatorics;
+
 public class PermutationTest {
 
+	@Test
+	@DisplayName("Select four of four")
+	public void testSelectFourOfFour() {
+		
+		int n = 4;
+		int m = 4;
+		int result = Combinatorics.permutations(n, m);
+		
+		assertThat(result, is(24));
+	}
+	
+	@Test
+	@DisplayName("Select three of five")
+	public void testSelectThreeOfFive() {
+		
+		int n = 5;
+		int m = 3;
+		int result = Combinatorics.permutations(n, m);
+		
+		assertThat(result, is(60));
+	}
 }
